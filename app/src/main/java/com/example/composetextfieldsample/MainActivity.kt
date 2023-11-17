cf59cdcda2eda18fbe9e62c05da1d671c4d85ce5
John Oberhauser <j.git-global@obez.io>
2023-11-17 09:50:15 -0600

    textfield

---
+++ app/src/main/java/com/example/composetextfieldsample/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.composetextfieldsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composetextfieldsample.ui.theme.ComposeTextfieldSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTextfieldSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val text = remember {
+                        mutableStateOf("")
+                    }
+                    TextField(
+                        modifier = Modifier.fillMaxSize(),
+                        value = text.value, 
+                        onValueChange = {
+                            text.value = it
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
